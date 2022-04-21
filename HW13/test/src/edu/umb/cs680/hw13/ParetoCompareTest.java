6e8bf6843279ab857ccaa497e43b60258d7484d4
qingyiwang001 <75802493+qingyiwang001@users.noreply.github.com>
2022-04-21 18:02:21 -0400

    homework13

---
+++ HW13/test/src/edu/umb/cs680/hw13/ParetoCompareTest.java
@@ -0,0 +1,69 @@
+package edu.umb.cs680.hw13;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import edu.umb.cs680.hw13.Car;
+
+public class ParetoCompareTest {
+	private static Car car1;
+	private static Car car2;
+	private static Car car3;
+	private static LinkedList<Car> usedCars;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		usedCars = new LinkedList<>();
+		car1 = new Car("Toyota", "RAV4", 70000, 2011, 7000.0f);
+		car2 = new Car("Honda", "CRV", 80000, 2010, 8000.0f);
+		car3 = new Car("Subaru", "FORESTER", 60000, 2012, 6000.0f);
+		usedCars.add(car1);
+		usedCars.add(car2);
+		usedCars.add(car3);
+	}
+	
+	@Test
+	public void dominationCountTestWithCar1() {
+		int expected = 1;
+		car1.setDominationCount(usedCars);
+		int actual = car1.getDominationCount();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void dominationCountTestWithCar2() {
+		int expected = 2;
+		car2.setDominationCount(usedCars);
+		int actual = car2.getDominationCount();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void dominationCountTestWithCar3() {
+		int expected = 0;
+		car3.setDominationCount(usedCars);
+		int actual = car3.getDominationCount();
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void dominationCountAscendingSortTest() {
+		LinkedList<Car> expected = new LinkedList<>();
+		expected.add(car3);
+		expected.add(car1);
+		expected.add(car2);
+		car1.setDominationCount(usedCars);
+		car2.setDominationCount(usedCars);
+		car3.setDominationCount(usedCars);
+		Collections.sort(usedCars, Comparator.comparing(car -> car.getDominationCount()));
+		Assert.assertArrayEquals(expected.toArray(), usedCars.toArray());
+	}
+
+}
